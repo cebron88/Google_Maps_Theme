2013b4e580c42ac6b8b869b97d888e047ce5a6fe
Christopher Ebron <76851827+cebron88@users.noreply.github.com>
2021-06-16 15:54:27 -0400

    progress

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+let map;
+function initMap() {
+ const options ={
+   zoom:15,
+   center:{lat:41.359770,lng:-72.109261},
+   mapId: '',                                     //FIX ME
+   
+ }
+ const map = new google.maps.Map(document.getElementById('map'),options)
 
+ const image = "snorlax.png";
+ 
+ const firstMarker = new google.maps.Marker({
+   position: {lat:41.35948, lng: -72.129261}, map,
+   icon: image,
+   
+ });
+}
